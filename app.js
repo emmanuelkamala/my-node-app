88a5096d85caccb622ffb1a3f3e6d4a4be53fcdb
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-01-04 06:21:39 +0300

    Add styles to the app

--- app.js
+++ app.js
@@ -5,14 +5,13 @@ const app = express();
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
+const notFoundRoutes = require('./routes/notFound')
 
 app.use(bodyParser.urlencoded({extended: false}));
 
 app.use(adminRoutes);
 app.use(shopRoutes);
+app.use(notFoundRoutes);
 
-app.use((req, res, next) => {
-  res.status(404).send('<h1>Page Not found</h1>')
-});
 
 app.listen(3000);
