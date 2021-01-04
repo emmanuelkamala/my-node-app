965cd43560fce338367dd8182379de6f3c61b414
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-01-04 07:11:37 +0300

    Install templating engines

--- app.js
+++ app.js
@@ -4,14 +4,14 @@ const bodyParser = require('body-parser');
 
 const app = express();
 
-const adminRoutes = require('./routes/admin');
+const adminData = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
 const notFoundRoutes = require('./routes/notFound')
 
 app.use(bodyParser.urlencoded({extended: false}));
 app.use(express.static(path.join(__dirname, 'public')))
 
-app.use(adminRoutes);
+app.use(adminData.routes);
 app.use(shopRoutes);
 app.use(notFoundRoutes);
 
