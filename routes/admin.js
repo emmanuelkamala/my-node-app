ef952a350a5ba341186c8a73949ba4ebf162353c
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-01-04 13:25:07 +0300

    Add dynamic content for the remaining pages

--- routes/admin.js
+++ routes/admin.js
@@ -7,7 +7,8 @@ const router = express.Router();
 const products = [];
 
 router.get('/add-product', (req, res, next) => {
-  res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  //res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
+  res.render('add-product')
 })
 
 router.post('/product', (req, res, next) => {
--- routes/notFound.js
+++ routes/notFound.js
@@ -5,7 +5,8 @@ const express = require('express');
 const router = express.Router();
 
 router.use((req, res, next) => {
-  res.status(404).sendFile(path.join(__dirname, '../', 'views', '404.html'));
+  //res.status(404).sendFile(path.join(__dirname, '../', 'views', '404.html'));
+  res.status(404).render('404');
 });
 
 module.exports = router;
