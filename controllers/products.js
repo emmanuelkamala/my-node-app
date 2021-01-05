46b22fb52b85255b615f3671d0accc6ee5cd57a6
emmanuelkamala <emmanuelkamala86@gmail.com>
2021-01-05 12:19:49 +0300

    Add product model to the app

--- controllers/products.js
+++ controllers/products.js
@@ -1,17 +1,24 @@
-const products = [];
+const Product = require('../models/Product');
 
 exports.getAddProduct = (req, res, next) => {
   //res.sendFile(path.join(__dirname, '../', 'views', 'add-product.html'));
-  res.render('add-product', {pageTitle: 'Add Product', path: '/add-product'});
+  res.render('add-product', {
+    pageTitle: 'Add Product', 
+    path: '/add-product'});
 }
 
 exports.postAddProduct = (req, res, next) => {
-  products.push({title: req.body.title})
+  const product = new Product(req.body.title);
+  product.save();
   res.redirect('/');
 }
 
 exports.getProducts = (req, res, next) => {
   // console.log(adminData.products);
   // res.sendFile(path.join(__dirname, '../', 'views', 'shop.html'))
-  res.render('shop', {prods: products, pageTitle: 'My Shop', path: '/'})
+  const products = Product.fetchAll();
+  res.render('shop', {
+    prods: products, 
+    pageTitle: 'My Shop', 
+    path: '/'})
 }
